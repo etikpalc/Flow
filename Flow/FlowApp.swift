6d4db59cda1f22499a8707a6d8d2c872ffcb6bb5
Leesu <tndusg7@gmail.com>
2024-04-09 15:15:15 +0900

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by 박수연 on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
